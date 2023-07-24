1f93830a3e92bca06e8438d288fd811441781662
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-24 14:23:41 -0700

    Added Delete Route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,8 +105,13 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const deleted = await Product.destroy({where: {id: req.params.id}});
+    res.json(deleted);
+  }catch (err) {
+    res.status(500).json({message: 'Product With That Id Not Found'})
+  }
 });
 
 module.exports = router;
