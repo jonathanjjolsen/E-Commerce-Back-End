7a659a6eab342d773fd470cb3708c6c19b8ac26d
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-24 13:36:47 -0700

    Updated Models index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: 'category_id'})
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey: 'category_id'})
 
 // Products belongToMany Tags (through ProductTag)
+// Through ProductTag Model
+Product.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+// Through ProductTag Model
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
