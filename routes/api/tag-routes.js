055dfe7c04a5f5b8b9aaa4b6533fc8e0b6952ad7
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-24 15:12:06 -0700

    Create Tag Route Completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,6 +3,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+//Get Tags
 router.get('/', async (req, res) => {
   try{
     const tags = await Tag.findAll({
@@ -26,14 +27,22 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+//Create Tag Route
+router.post('/', async (req, res) => {
+  try{
+    const newTag = await Tag.create(req.body);
+    res.json(newTag);
+  }catch(err){
+    res.status(500).json({message: 'Could Not Create New Tag, Please Try Again.'})
+  }
 });
 
+//Update A Single Tag by passing the ID number through the URL
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
+//Delete A Single Tag by passing the ID number through the URL
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
 });
